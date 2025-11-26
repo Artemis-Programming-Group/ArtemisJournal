c80d0f8602cca86627427eb5c96826984802898d
alireza hadizadeh <alireza.hadizadeh25@yahoo.com>
2025-11-26 16:57:53 +0330

    add Favicon

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#0f172a">
 
+    <link rel="icon" type="image/x-icon" href="/icons/icon-192x192.png">
+
     <link rel="manifest" href="{{ url('/manifest.webmanifest') }}">
 
     <link rel="apple-touch-icon" href="/icons/icon-192x192.png">
